9411f2b464c789801a935a099ad0130ac51b6bb5
AccurateApplication <lucas@cederqvist.dev>
2022-11-09 23:42:02 +0100

    exercise 3
    skipped first word check due to getting stuck. maybe later.

--- wk1/3.rkt
+++ wk1/3.rkt
@@ -5,27 +5,29 @@
 ;; (Don’t worry about capitalization of letters.) Example:
 ;; > (switch ’(You told me that I should wake you up))
 ;; (i told you that you should wake me up)
-;; Check if first
-(define (checkFirstWord word)
-  (if (word "you")
-      ("I") ;; true
-  (if (word "You")
-      ("I") ;; true
-      (word)))) ;; else
 
 (define (checkWord word)
-  (if (= word "you")
-      ("me") ;; true
-  (if (word "i")
-      ("you")
-  (if (word "me")
-      ("you")
-      (word)))))
+  (if (= word 'you)
+      ('me)
+      word)
+  (if (= word 'i)
+      ('you)
+      word)
+  (if (= word 'me)
+      ('you)
+      (word)))
 
-(define (replaceme sentence)
-  (if (empty? sentence)
+(define (switch-word w)
+  (cond ((equal? w 'i)  'you)
+        ((equal? w 'me) 'you)
+        ((equal? w 'you) 'me)
+        (else w)))
+
+(define (replacemesen phrase)
+  (if (empty? phrase)
     '()
-    (se (checkFirstWord (first sentence))
-        (replaceme  (checkWord (butfirst sentence))))))
+    (sentence (switch-word (first phrase))
+              (replacemesen (butfirst phrase)))))
 
-(replaceme '(You told you that you should wake me up))
+(replacemesen '(you told you that you should wake me up))
+(replacemesen '(i told me that i should wake me up))
